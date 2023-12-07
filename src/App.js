8f74ca108260f47fb44c22011662dccd73f770b8
aislyndev <aaislyndev@gmail.com>
2023-12-07 18:34:44 +0300

    react

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import './images/nationalgeographic.jpg'
+
+
+
+function App() {
+  return (
+    <>
+    <nav><h1>DOĞA FOTOĞRAFLARIM</h1>
+    <ul>
+      <li>Main Page</li>
+      <li>Project</li>
+      <li>About Us</li>
+      <li>Contact</li>
+    </ul>
+    
+    </nav>
+
+    <section className="backimage"></section>
+
+    </>
+  );
+}
+
+export default App;
